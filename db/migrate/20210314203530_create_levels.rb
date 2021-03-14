db0e151c063f2c316bc6e208b2b69f8651bd303d
Ranko Liang <rankoliang@gmail.com>
2021-03-14 16:36:05 -0400

    Add level model

---
+++ db/migrate/20210314203530_create_levels.rb
@@ -0,0 +1,10 @@
+class CreateLevels < ActiveRecord::Migration[6.1]
+  def change
+    create_table :levels do |t|
+      t.string :title
+      t.text :image_src
+
+      t.timestamps
+    end
+  end
+end
