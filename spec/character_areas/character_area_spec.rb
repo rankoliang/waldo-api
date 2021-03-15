22642943b8fe2592933ccea8a8acbd8c53c9fe55
Ranko Liang <rankoliang@gmail.com>
2021-03-15 16:31:18 -0400

    Make character_area_spec more explicit
    The specs were not scoped to the for class method.

--- spec/character_areas/character_area_spec.rb
+++ spec/character_areas/character_area_spec.rb
@@ -2,25 +2,27 @@ require_relative '../../app/character_areas/character_area'
 require_relative '../../app/character_areas/character_rect_area'
 
 RSpec.describe CharacterArea do
-  subject(:area) { described_class.for(shape: shape, coordinates: coordinates) }
+  describe '.for' do
+    subject(:area) { described_class.for(shape: shape, coordinates: coordinates) }
 
-  context 'when the shape is a rect' do
-    let(:shape) { 'rect' }
-    let(:coordinates) { '0,0,10,10' }
+    context 'when the shape is a rect' do
+      let(:shape) { 'rect' }
+      let(:coordinates) { '0,0,10,10' }
 
-    it 'returns a CharacterRectArea' do
-      expect(area).to be_kind_of(CharacterRectArea)
+      it 'returns a CharacterRectArea' do
+        expect(area).to be_kind_of(CharacterRectArea)
+      end
     end
-  end
 
-  context 'when the shape is unknown' do
-    let(:shape) { 'invalid' }
-    let(:coordinates) { 'invalid' }
+    context 'when the shape is unknown' do
+      let(:shape) { 'invalid' }
+      let(:coordinates) { 'invalid' }
 
-    it {
-      expect do
-        described_class.for(shape: shape, coordinates: coordinates)
-      end.to raise_error(ArgumentError, "no character area for #{shape}")
-    }
+      it {
+        expect do
+          described_class.for(shape: shape, coordinates: coordinates)
+        end.to raise_error(ArgumentError, "no character area for #{shape}")
+      }
+    end
   end
 end
