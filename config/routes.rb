b48309309789791bd18e104208525225dde29f2b
Ranko Liang <rankoliang@gmail.com>
2021-03-15 14:10:40 -0400

    Add nested routes for level

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :levels, only: %i[index show]
+      resources :levels, only: %i[index show] do
+        resources :scores, only: %i[index create], path: 'leaderboard', as: 'leaderboard'
+        resources :characters, only: [:index]
+        get '/characters/:id/search', to: 'characters#search', as: 'search'
+      end
     end
   end
 end
