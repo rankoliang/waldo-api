739e4025b3d6708037db3a4ff3e31cf3ae7d4fb0
Ranko Liang <rankoliang@gmail.com>
2021-03-15 17:18:03 -0400

    Add character search response

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -11,7 +11,17 @@ class Api::V1::CharactersController < ApplicationController
     character = Character.find(params[:id])
 
     raise ActiveRecord::RecordNotFound unless character.level_id == params[:level_id].to_i
+
+    render json: { found: character.found?(**coordinate_params) }
   rescue ActiveRecord::RecordNotFound
     render json: { error: 'The character was not found' }, status: :not_found
+  rescue ArgumentError
+    render json: { error: 'missing coordinate' }, status: :bad_request
+  end
+
+  private
+
+  def coordinate_params
+    params.permit(:x, :y).to_h.symbolize_keys.transform_values(&:to_i)
   end
 end
--- spec/requests/api/v1/characters_spec.rb
+++ spec/requests/api/v1/characters_spec.rb
@@ -39,8 +39,25 @@ RSpec.describe 'Api::V1::Characters', type: :request do
     context 'when the level is found' do
       context 'when the character is found' do
         it 'returns http success' do
-          get api_v1_level_search_path(level, character)
+          get api_v1_level_search_path(level, character), params: { x: 5, y: 5 }
+
           expect(response).to have_http_status(:success)
+        end
+
+        it 'returns whether the character is found' do
+          get api_v1_level_search_path(level, character), params: { x: 5, y: 5 }
+
+          data = JSON.parse(response.body, symbolize_names: true)
+
+          expect(data).to include(:found)
+        end
+
+        context 'when incorrect parameters are passed' do
+          it 'returns http bad request' do
+            get api_v1_level_search_path(level, character)
+
+            expect(response).to have_http_status(:bad_request)
+          end
         end
       end
 
