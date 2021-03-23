e885a6cbb4c3342cd568cb4a40f4577d9a05060d
Ranko Liang <rankoliang@gmail.com>
2021-03-23 17:10:27 -0400

    Remove coordinate params method from search

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -12,16 +12,10 @@ class Api::V1::CharactersController < ApplicationController
 
     raise ActiveRecord::RecordNotFound unless character.level_id == params[:level_id].to_i
 
-    render json: { found: character.found?(**coordinate_params) }
+    render json: { found: character.found?(x: params[:x].to_i, y: params[:y].to_i) }
   rescue ActiveRecord::RecordNotFound
     render json: { error: 'The character was not found' }, status: :not_found
   rescue ArgumentError
-    render json: { error: 'missing coordinate' }, status: :bad_request
-  end
-
-  private
-
-  def coordinate_params
-    params.permit(:x, :y).to_h.symbolize_keys.transform_values(&:to_i)
+    render json: { error: 'Incorrect coordinate format' }, status: :bad_request
   end
 end
