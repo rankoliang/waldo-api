b32e7a14e013690aa2cd359f2cbff7575abd1123
Ranko Liang <rankoliang@gmail.com>
2021-03-29 22:42:28 -0400

    Update character search response

--- app/controllers/api/v1/search_areas_controller.rb
+++ app/controllers/api/v1/search_areas_controller.rb
@@ -16,10 +16,18 @@ class Api::V1::SearchAreasController < ApplicationController
       data['end_time'] = Time.current if characters_found.values.all?(true)
     end
 
-    render json: { found: found, token: token }
+    render json: { found: found, token: token, duration: milliseconds_elapsed(token) }
   rescue ActiveRecord::RecordNotFound
     render json: { error: 'The character was not found' }, status: :not_found
   rescue ArgumentError
     render json: { error: 'Incorrect coordinate format' }, status: :bad_request
+  end
+
+  private
+
+  def milliseconds_elapsed(token)
+    data = decrypt_and_verify(token)
+
+    ((data['end_time'] - data['start_time']) * 1000).ceil if data['end_time']
   end
 end
